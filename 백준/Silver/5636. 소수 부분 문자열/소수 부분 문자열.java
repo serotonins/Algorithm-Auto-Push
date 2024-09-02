746820088c4772e4fc736ce2acb8f716545c3257
serotonins <89008498+serotonins@users.noreply.github.com>
2024-09-02 16:06:56 +0900

    [Silver I] Title: 소수 부분 문자열, Time: 60 ms, Memory: 11548 KB -BaekjoonHub

--- 백준/Silver/5636. 소수 부분 문자열/소수 부분 문자열.java
+++ 백준/Silver/5636. 소수 부분 문자열/소수 부분 문자열.java
@@ -3,36 +3,36 @@ import java.io.*;
 
 public class Main {
 
+    public static boolean isPrime(int num) {
+        if (num < 2) return false;
+        for (int i = 2; i <= Math.sqrt(num); i++) {
+            if (num % i == 0) return false;
+        }
+        return true;
+    }
+
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
 
-        boolean[] sosu = new boolean[100001];
-        Arrays.fill(sosu, true);
-        sosu[0] = sosu[1] = false;
-        for (int i = 2; i < Math.sqrt(100000) + 1; i++) {
-            if (!sosu[i]) continue;
-            for (int j = i * i; j < 100001; j += i) {
-                sosu[j] = false;
-            }
-        }
-
         while (true) {
             String s = br.readLine();
             if (s.equals("0")) break;
-            int start = 100000;
             int answer = 0;
-            if (s.length() < 6) {
-                start = Integer.parseInt(s);
-            }
 
-            for (int i = start; i > 1; i--) {
-                if (sosu[i] && s.contains(i + "")) {
-                    answer = i;
+            for (int l = Math.min(s.length(), 5); l > 0; l--) {
+                for (int i = 0; i <= s.length() - l; i++) {
+                    int temp = Integer.parseInt(s.substring(i, i+l));
+                    if (isPrime(temp)) {
+                        answer = Math.max(answer, temp);
+                    }
+                }
+                if (answer != 0) {
                     break;
                 }
             }
+
             bw.append(answer+"\n");
         }
 
