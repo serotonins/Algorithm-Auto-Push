033d7a367b260c7dfdc7848f899552e105186148
serotonins <89008498+serotonins@users.noreply.github.com>
2024-12-09 20:41:35 +0900

    [Gold IV] Title: 부분합, Time: 208 ms, Memory: 23028 KB -BaekjoonHub

--- 백준/Gold/1806. 부분합/부분합.java
+++ 백준/Gold/1806. 부분합/부분합.java
@@ -3,13 +3,18 @@ import java.io.*;
 
 public class Main {
 
+//    static int n, m;
     static int n, s, answer, inf = Integer.MAX_VALUE;
     static int[] sequence, partialSum;
+//    static int[][] dr = {{-1,0,1,0}, {0,1,0,-1}};
+//
+//    static boolean isOut(int y, int x) {
+//        return y < 0 || y >= n || x < 0 || x >= m;
+//    }
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
 
         n = Integer.parseInt(st.nextToken());
@@ -32,18 +37,24 @@ public class Main {
             partialSum[i] = partialSum[i-1] + sequence[i];
         }
 
-        for (int i = -1; i < n-2; i++) {
-            for (int j = i+2; j < n; j++) {
-                if (j-i >= answer) break;
-                int sum = partialSum[j];
-                if (i != -1) sum -= partialSum[i];
-                if (sum >= s) {
-                    answer = Math.min(j-i, answer);
-                    break;
-                }
+        int start = -1;
+        int end = 1;
+        while (start <= end && end < n) {
+            int minus = 0;
+            if (start != -1) minus = partialSum[start];
+            int now = partialSum[end] - minus;
+            if (now > s) {
+                answer = Math.min(answer, end-start);
+                start++;
+            } else if (now <= s) {
+                if (now == s) {answer = Math.min(answer, end-start);}
+                end++;
+                if (end-start >= answer) start = end-answer+1;
             }
         }
 
         System.out.println(answer == inf ? 0 : answer);
+
     }
 }
+
