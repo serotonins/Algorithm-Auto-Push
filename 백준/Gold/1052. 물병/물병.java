2b9532804fed5648573b2d70c862e93e37b8ebe0
serotonins <89008498+serotonins@users.noreply.github.com>
2024-12-17 23:08:30 +0900

    [Gold V] Title: 물병, Time: 412 ms, Memory: 11712 KB -BaekjoonHub

---
+++ 백준/Gold/1052. 물병/물병.java
@@ -0,0 +1,40 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+	static int n, m;
+
+	
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		
+		st = new StringTokenizer(br.readLine());
+		n = Integer.parseInt(st.nextToken());
+		m = Integer.parseInt(st.nextToken());
+		
+		int plus = 0;
+		
+		int result = -1;
+		
+		while (true) {
+			int s = n + plus;
+			int cnt = 0;
+
+			while (s > 0) {
+				if (s % 2 != 0) cnt++;
+				s /= 2;
+			}
+			
+			if (cnt <= m) {
+				result = plus;
+				break;
+			}
+			else plus++;
+		}
+		
+		
+		System.out.println(result);
+		
+	}
+}
