64bb05353f79c10a064b2d3ff8099e4377afaad9
serotonins <89008498+serotonins@users.noreply.github.com>
2024-12-21 21:57:07 +0900

    [level 1] Title: [PCCE 기출문제] 9번 / 지폐 접기, Time: 0.07 ms, Memory: 78.2 MB -BaekjoonHub

--- 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
+++ 프로그래머스/1/340199. ［PCCE 기출문제］ 9번 ／ 지폐 접기/［PCCE 기출문제］ 9번 ／ 지폐 접기.java
@@ -1,25 +1,15 @@
 import java.util.*;
 
 class Solution {
+    public int min(int[] arr) {return Math.min(arr[0], arr[1]);}
+    public int max(int[] arr) {return Math.max(arr[0], arr[1]);}
     public int solution(int[] wallet, int[] bill) {
         int answer = 0;
-        
-        Integer[] wallets = new Integer[2];
-        for (int i = 0; i < 2; i++) {wallets[i] = wallet[i];}
-        Integer[] bills = new Integer[2];
-        for (int i = 0; i < 2; i++) {bills[i] = bill[i];}
-        
-        Arrays.sort(wallets, Comparator.reverseOrder());
-        
         while (true) {
-            Arrays.sort(bills, Comparator.reverseOrder());
-            System.out.println(Arrays.toString(wallet) + Arrays.toString(bills));
-            if (wallets[0] >= bills[0] && wallets[1] >= bills[1]) {break;}
-            bills[0] /= 2;
-            System.out.println(Arrays.toString(bills));
+            if (max(wallet) >= max(bill) && min(wallet) >= min(bill)) {break;}
+            bill[bill[0] > bill[1] ? 0 : 1] /= 2;
             answer++;
         }
-        
         return answer;
     }
 }
