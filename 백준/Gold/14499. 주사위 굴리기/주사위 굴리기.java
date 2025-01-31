629ae926f17f18b329234ca0829d709265bce9e4
serotonins <89008498+serotonins@users.noreply.github.com>
2025-01-31 11:35:54 +0900

    [Gold IV] Title: 주사위 굴리기, Time: 72 ms, Memory: 11976 KB -BaekjoonHub

--- 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
+++ 백준/Gold/14499. 주사위 굴리기/주사위 굴리기.java
@@ -2,73 +2,14 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n, m, k;
+    static int n, m, k, y, x;
 
     static int[][] dr = {{0,0,0,-1,1}, {0,1,-1,0,0}};
-
+    static int[][] ver = {{0,1,3,6,4}, {0,1,5,6,2}};
     static boolean isOut(int y, int x) {return y < 0 || y >= n || x < 0 || x >= m;}
 
     static int[][] map;
 
-    static class Dice {
-        int top = 0, up = 0, right = 0, down = 0, left = 0, bottom = 0;
-        int y, x;
-        public Dice(int y, int x) {
-            this.y = y;
-            this.x = x;
-        }
-        public Dice roll(int d) {
-            if (isOut(y+dr[0][d], x+dr[1][d])) return this;
-
-            if (d == 1) east();
-            else if (d == 2) west();
-            else if (d == 3) north();
-            else south();
-
-            if (map[y][x] == 0) map[y][x] = bottom;
-            else {
-                bottom = map[y][x];
-                map[y][x] = 0;
-            }
-
-            System.out.println(top);
-
-            return this;
-        }
-
-        public void east() {
-            int temp = top;
-            top = left;
-            left = bottom;
-            bottom = right;
-            right = temp;
-            x++;
-        }
-        public void west() {
-            int temp = top;
-            top = right;
-            right = bottom;
-            bottom = left;
-            left = temp;
-            x--;
-        }
-        public void north() {
-            int temp = top;
-            top = down;
-            down = bottom;
-            bottom = up;
-            up = temp;
-            y--;
-        }
-        public void south() {
-            int temp = top;
-            top = up;
-            up = bottom;
-            bottom = down;
-            down = temp;
-            y++;
-        }
-    }
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -77,7 +18,8 @@ public class Main {
 
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
-        Dice dice = new Dice(Integer.parseInt(st.nextToken()), Integer.parseInt(st.nextToken()));
+        y = Integer.parseInt(st.nextToken());
+        x = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
 
         map = new int[n][m];
@@ -89,10 +31,32 @@ public class Main {
             }
         }
 
+        int[] dice = new int[7];
+
         st = new StringTokenizer(br.readLine());
         for (int t = 0; t < k; t++) {
-            dice.roll(Integer.parseInt(st.nextToken()));
+            int d = Integer.parseInt(st.nextToken());
+            if (isOut(y+dr[0][d], x+dr[1][d])) continue;
+            if (d == 1 || d == 4) {
+                for (int i = 4; i >= 0; i--) {
+                    dice[ver[d/3][(1+i)%5]] = dice[ver[d/3][i]];
+                }
+            } else {
+                for (int i = 0; i < 5; i++) {
+                    dice[ver[d/3][i]] = dice[ver[d/3][(i+1)%5]];
+                }
+            }
+            y+=dr[0][d];
+            x+=dr[1][d];
+            if (map[y][x] == 0) map[y][x] = dice[6];
+            else {
+                dice[6] = map[y][x];
+                map[y][x] = 0;
+            }
+            sb.append(dice[1]+"\n");
         }
+
+        System.out.println(sb.toString());
     }
 }
 
