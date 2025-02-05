92e5432fc7f5f8deb732ebc28ed2d22c12060f59
serotonins <89008498+serotonins@users.noreply.github.com>
2025-02-05 16:21:12 +0900

    [Gold III] Title: 내리막 길, Time: 120 ms, Memory: 15152 KB -BaekjoonHub

--- 백준/Gold/1520. 내리막 길/내리막 길.java
+++ 백준/Gold/1520. 내리막 길/내리막 길.java
@@ -3,10 +3,18 @@ import java.io.*;
 
 public class Main {
     static int n, m;
-
     static int[][] dr = {{0,0,-1,1}, {1,-1,0,0}};
     static int[][] dp, map;
     static boolean isOut(int y, int x) {return y < 0 || y >= n || x < 0 || x >= m;}
+
+    static int read() throws IOException {
+        int n = System.in.read() & 15;
+        int c;
+        while ((c = System.in.read()) > 32) {
+            n = (n << 3) + (n << 1) + (c & 15);
+        }
+        return n;
+    }
 
     static int dfs(int y, int x) {
         if (y == n-1 && x == m-1) return 1;
@@ -23,20 +31,16 @@ public class Main {
     }
 
     public static void main(String[] args) throws IOException {
-
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-
-        n = Integer.parseInt(st.nextToken());
-        m = Integer.parseInt(st.nextToken());
+        
+        n = read();
+        m = read();
 
         map = new int[n][m];
         dp = new int[n][m];
 
         for (int i = 0; i < n; i++) {
-            st = new StringTokenizer(br.readLine());
             for (int j = 0; j < m; j++) {
-                map[i][j] = Integer.parseInt(st.nextToken());
+                map[i][j] = read();
                 dp[i][j] = -1;
             }
         }
