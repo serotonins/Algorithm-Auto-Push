0015625b2108f931e0a960260460b4cb1f793607
serotonins <89008498+serotonins@users.noreply.github.com>
2025-02-17 23:29:07 +0900

    [Gold IV] Title: 이중 우선순위 큐, Time: 2288 ms, Memory: 440020 KB -BaekjoonHub

--- 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
+++ 백준/Gold/7662. 이중 우선순위 큐/이중 우선순위 큐.java
@@ -2,57 +2,44 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-	static int n;
+    static int n;
+    static char input = 'I', delete = 'D';
+    static StringBuilder sb = new StringBuilder();
+    static TreeMap<Integer, Integer> map = new TreeMap<>();
 
-	
-	static TreeSet<Integer> fset = new TreeSet<>();
+    public static void main(String[] args) throws IOException {
 
-    static Map<Integer, Integer> map;
-	
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
-		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-		
-		int T = Integer.parseInt(st.nextToken());
-        for (int tc = 0; tc < T; tc++) {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+
+        int T = Integer.parseInt(br.readLine());
+        for (int t = 0; t < T; t++) {
             n = Integer.parseInt(br.readLine());
-            fset = new TreeSet<>();
-            map = new HashMap<Integer, Integer>();
-            for (int l = 0; l < n; l++) {
+            map.clear();
+            for (int i = 0; i < n; i++) {
                 st = new StringTokenizer(br.readLine());
-                char inst = st.nextToken().charAt(0);
-                int num = Integer.parseInt(st.nextToken());
-                
-                if (inst == 'D') {
-                	if (fset.size() == 0) continue;
-                	
-                	int find = fset.last();
-                    if (num == -1) {
-                        find = fset.first();
-                    } 
-                    
-                    int cnt = map.get(find);
-                    if (cnt == 1) {
-                    	fset.remove(find);
-                    	map.remove(find);
-                    } else {
-                    	map.put(find, cnt-1);
-                    }
-                } else {
-                	if (fset.contains(num)) {
-                		map.put(num, map.get(num)+1);
-                	} else {
-                		fset.add(num);
-                        map.put(num, 1);
-                	}
-                    
+                if (st.nextToken().charAt(0) == input) {
+                    int num = Integer.parseInt(st.nextToken());
+                    map.put(num, map.getOrDefault(num, 0)+1);
+                } else if (map.size() != 0){
+                    int num = Integer.parseInt(st.nextToken());
+                    if (num == -1) num = map.firstKey();
+                    else num = map.lastKey();
+                    if (map.get(num) == 1) map.remove(num);
+                    else map.put(num, map.get(num)-1);
                 }
             }
-            
-            if (fset.size() == 0) System.out.println("EMPTY");
-            else System.out.println(fset.last() + " " + fset.first());
+            if (map.isEmpty()) sb.append("EMPTY\n");
+            else {
+                sb.append(map.lastKey());
+                sb.append(" ");
+                sb.append(map.firstKey());
+                sb.append("\n");
+            }
         }
-		
-	}
+
+        System.out.println(sb.toString());
+
+    }
 }
+
