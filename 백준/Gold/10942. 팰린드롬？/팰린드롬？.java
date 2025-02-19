2dbba0c81823a2cc55e6152c7a4e756f2c29bfdf
serotonins <89008498+serotonins@users.noreply.github.com>
2025-02-19 16:41:17 +0900

    [Gold IV] Title: 팰린드롬?, Time: 304 ms, Memory: 29872 KB -BaekjoonHub

--- 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
+++ 백준/Gold/10942. 팰린드롬？/팰린드롬？.java
@@ -5,6 +5,7 @@ public class Main {
     static int n, m, k, answer, len, inf = Integer.MAX_VALUE;
     static StringBuilder sb = new StringBuilder();
     static int[] arr;
+    static boolean[][] dp;
 
     static int read() throws IOException {
         int c, n = System.in.read() & 15;
@@ -14,29 +15,38 @@ public class Main {
         return n;
     }
 
+    static void setting() {
+        for (int i = 1; i <= n; i++) {
+            dp[i][i] = true;
+        }
+        for (int i = 1; i < n; i++) {
+            if (arr[i] == arr[i+1]) dp[i][i+1] = true;
+        }
+        for (int i = 2; i < n; i++) {
+            for (int j = 1; j <= n-i; j++) {
+                if (dp[j+1][j+i-1] && arr[j] == arr[j+i]) dp[j][j+i] = true;
+            }
+        }
+    }
+
     public static void main(String[] args) throws IOException {
 
         n = read();
         arr = new int[n+1];
+        dp = new boolean[n+1][n+1];
         for (int i = 1; i <= n; i++) {
             arr[i] = read();
         }
+        setting();
+
         m = read();
         for (int t = 0; t < m; t++) {
             int s = read();
             int e = read();
-            boolean pal = true;
-            for (int i = 0; i < (e-s+1)/2; i++) {
-                if (arr[s+i] != arr[e-i]) {
-                    pal = false;
-                    break;
-                }
-            }
-            sb.append(pal ? 1 : 0);
-            sb.append("\n");
+            sb.append(dp[s][e] ? "1\n" : "0\n");
         }
 
-        System.out.println(sb.toString());
+        System.out.print(sb.toString());
     }
 }
 
