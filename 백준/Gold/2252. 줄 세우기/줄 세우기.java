fd974f0ea4893bfd16861488883af0d75c6f255d
serotonins <89008498+serotonins@users.noreply.github.com>
2025-02-20 16:20:39 +0900

    [Gold III] Title: 줄 세우기, Time: 256 ms, Memory: 25232 KB -BaekjoonHub

--- 백준/Gold/2252. 줄 세우기/줄 세우기.java
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.java
@@ -4,9 +4,9 @@ import java.io.*;
 public class Main {
     static int n, m, k, answer, len, inf = Integer.MAX_VALUE;
     static StringBuilder sb = new StringBuilder();
-    static boolean[] visit;
+    static int[] parents;
     static Set<Integer> heads;
-    static ArrayList<Integer>[] fronts, backs;
+    static ArrayList<Integer>[] backs;
 
     static int read() throws IOException {
         int c, n = System.in.read() & 15;
@@ -19,31 +19,14 @@ public class Main {
     static void setting() throws IOException {
         for (int i = 1; i < n+1; i++) {
             heads.add(i);
-            fronts[i] = new ArrayList<>();
             backs[i] = new ArrayList<>();
         }
         for (int i = 0; i < m; i++) {
             int f = read();
             int b = read();
-            fronts[b].add(f);
             backs[f].add(b);
             heads.remove(b);
-        }
-    }
-
-    static void running(int now) {
-        for (int i : fronts[now]) {
-            if (visit[i]) continue;
-            running(i);
-        }
-        if (!visit[now]) {
-            sb.append(now);
-            sb.append(" ");
-            visit[now] = true;
-        }
-        for (int i : backs[now]) {
-            if (visit[i]) continue;
-            running(i);
+            parents[b]++;
         }
     }
 
@@ -51,21 +34,23 @@ public class Main {
 
         n = read();
         m = read();
-        visit = new boolean[n+1];
+        parents = new int[n+1];
         heads = new HashSet<>();
-        fronts = new ArrayList[n+1];
         backs = new ArrayList[n+1];
         setting();
 
-        for (int s : heads) {
-            if (backs[s].isEmpty()) {
-                sb.append(s);
-                sb.append(" ");
-                visit[s] = true;
-                continue;
-            } else if (visit[s]) continue;
-            running(s);
+        Queue<Integer> que = new LinkedList<>(heads);
+
+        while (!que.isEmpty()) {
+            int now = que.poll();
+            sb.append(now).append(" ");
+            for (int i : backs[now]) {
+                if (--parents[i] == 0) {
+                    que.add(i);
+                }
+            }
         }
+
         System.out.println(sb.toString());
     }
 }
