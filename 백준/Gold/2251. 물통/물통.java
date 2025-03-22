26eab98fa50750ca98468aa974af83dd08857ea5
serotonins <89008498+serotonins@users.noreply.github.com>
2025-03-22 22:32:11 +0900

    [Gold IV] Title: 물통, Time: 76 ms, Memory: 20696 KB -BaekjoonHub

--- 백준/Gold/2251. 물통/물통.java
+++ 백준/Gold/2251. 물통/물통.java
@@ -2,7 +2,7 @@ import java.util.*;
 import java.io.*;
 
 public class Main {
-    static int n, m, f, k, answer, len, inf = Integer.MAX_VALUE;
+    static int[] lim;
     static TreeSet<Integer> set = new TreeSet<>();
     static StringBuilder sb = new StringBuilder();
     
@@ -15,81 +15,44 @@ public class Main {
     }
 
     static class Water {
-        int a,b,c;
-        public Water(int a, int b, int c) {
-            this.a = a;
-            this.b = b;
-            this.c = c;
+        int[] abc = new int[4];
+        public Water(int[] abc) {
+            for (int i = 1; i < 4; i++) {
+                this.abc[i] = abc[i];
+            }
         }
     }
 
     static Water pour(Water now, int dep, int arr) {
-        if (dep == 1) {
-            if (arr == 2) {
-                if (now.a + now.b > m) {
-                    return new Water(now.a - (m - now.b), m, now.c);
-                } else {
-                    return new Water(0, now.b + now.a, now.c);
-                }
-            } else {
-                if (now.a + now.c > k) {
-                    return new Water(now.a - (k - now.c), now.b, k);
-                } else {
-                    return new Water(0, now.b, now.c + now.a);
-                }
-            }
-        } else if (dep == 2) {
-            if (arr == 1) {
-                if (now.a + now.b > n) {
-                    return new Water(n, now.b - (n - now.a), now.c);
-                } else {
-                    return new Water(now.a + now.b, 0, now.c);
-                }
-            } else {
-                if (now.b + now.c > k) {
-                    return new Water(now.a, now.b - (k - now.c), k);
-                } else {
-                    return new Water(now.a, 0, now.c + now.b);
-                }
-            }
-        } else {
-            if (arr == 1) {
-                if (now.a + now.c > n) {
-                    return new Water(n, now.b, now.c - (n - now.a));
-                } else {
-                    return new Water(now.a + now.c, now.b, 0);
-                }
-            } else {
-                if (now.b + now.c > m) {
-                    return new Water(now.a, m, now.c - (m - now.b));
-                } else {
-                    return new Water(now.a, now.b + now.c, 0);
-                }
-            }
-        }
+        Water next = new Water(now.abc);
+        int dir = Math.min(lim[arr] - now.abc[arr], now.abc[dep]);
+        next.abc[dep] -= dir;
+        next.abc[arr] += dir;
+        return next;
     }
 
     public static void main(String[] args) throws IOException {
+        
+        lim = new int[4];
+        for (int i = 1; i < 4; i++) {
+            lim[i] = read();
+        }
 
-        n = read();
-        m = read();
-        k = read();
-
-        boolean[][][] visit = new boolean[n+1][m+1][k+1];
-        visit[0][0][k] = true;
+        boolean[][][] visit = new boolean[lim[1]+1][lim[2]+1][lim[3]+1];
+        visit[0][0][lim[3]] = true;
 
         Queue<Water> que = new LinkedList<>();
-        que.add(new Water(0, 0, k));
-        set.add(k);
+        que.add(new Water(new int[] {0,0,0,lim[3]}));
+        set.add(lim[3]);
         while (!que.isEmpty()) {
             Water now = que.poll();
             for (int d = 1; d <= 3; d++) {
                 for (int a = 1; a <= 3; a++) {
                     if (d == a) continue;
                     Water next = pour(now, d, a);
-                    if (visit[next.a][next.b][next.c]) continue;
-                    visit[next.a][next.b][next.c] = true;
-                    if (next.a == 0) set.add(next.c);
+                    if (visit[next.abc[1]][next.abc[2]][next.abc[3]]) continue;
+                    visit[next.abc[1]][next.abc[2]][next.abc[3]] = true;
+                    if (next.abc[1] == 0) set.add(next.abc[3]);
                     que.add(next);
                 }
             }
