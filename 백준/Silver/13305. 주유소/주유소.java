8b2e6c74ccd6724bc1d64425556d91021d30cb5e
serotonins <89008498+serotonins@users.noreply.github.com>
2025-06-28 23:25:24 +0900

    [Silver III] Title: 주유소, Time: 248 ms, Memory: 31396 KB, Score: 58 point -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,37 @@
+import java.util.*;
+import java.io.*;
+
+public class Main {
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static StringTokenizer st;
+
+    static void write(int[] arr) throws IOException {
+        st = new StringTokenizer(br.readLine());
+        int len = arr.length;
+        for (int i = 0; i < len; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+    }
+
+    public static void main(String[] args) throws IOException {
+        
+        int n = Integer.parseInt(br.readLine());
+        int[] distance = new int[n-1];
+        int[] price = new int[n];
+
+        write(distance);
+        write(price);
+
+
+        int minPrice = Integer.MAX_VALUE;
+        int answer = 0;
+        for (int i = 0; i < n - 1; i++) {
+            minPrice = Math.min(minPrice, price[i]);
+            answer += minPrice * distance[i];
+        }
+
+        System.out.println(answer);
+
+    }
+}
+
