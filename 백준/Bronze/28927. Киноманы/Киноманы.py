138ee8c11adb28fc049bad8f94bd540cf3b56d9e
serotonins <89008498+serotonins@users.noreply.github.com>
2025-08-22 19:42:27 +0900

    [Bronze V] Title: Киноманы, Time: 112 ms, Memory: 108384 KB -BaekjoonHub

---
+++ 백준/Bronze/28927. Киноманы/Киноманы.py
@@ -0,0 +1,6 @@
+t1, e1, f1 = map(int, input().split())
+t2, e2, f2 = map(int, input().split())
+
+if t1 * 3 +e1 * 20 + f1 * 120 > t2 * 3 + e2 * 20 +f2 * 120 :print("Max")
+elif t1 * 3 +e1 * 20 +f1 * 120 == t2 * 3+ e2 * 20+f2 * 120 :print("Draw")
+else: print("Mel")
