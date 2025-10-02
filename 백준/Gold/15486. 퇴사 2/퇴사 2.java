5a22d47a87b7da374cc523779b0385ab5f94e577
serotonins <89008498+serotonins@users.noreply.github.com>
2025-10-02 22:56:00 +0900

    [Gold V] Title: 퇴사 2, Time: 292 ms, Memory: 18048 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -2,7 +2,7 @@ import java.io.IOException;
 
 public class Main {
     static int n;
-    static StringBuilder sb = new StringBuilder();
+    
     static int read() throws IOException {
         int c, n = System.in.read() & 15;
         while ((c = System.in.read()) > 32) {
@@ -14,19 +14,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         n = read();
 
-        int[][] works = new int[n+1][2];
+        int[] dp = new int[n+1];
         for (int i = 1; i <= n; i++) {
-            for (int j = 0; j < 2; j++) {
-                works[i][j] = read();
+            int time = read();
+            int pay = read();
+            
+            dp[i] = Math.max(dp[i], dp[i-1]);
+            if (i + time <= n + 1) {
+                dp[i + time - 1] = Math.max(dp[i + time - 1], dp[i - 1] + pay);
             }
         }
 
-        int[] dp = new int[n+51];
-        for (int i = n; i >= 1; i--) {
-            dp[i] = Math.max(dp[i], dp[i+1]);
-            if (i + works[i][0] <= n + 1) dp[i] = Math.max(dp[i], dp[i + works[i][0]] + works[i][1]);
-        }
-
-        System.out.println(dp[1]);
+        System.out.println(dp[n]);
     }
 }
