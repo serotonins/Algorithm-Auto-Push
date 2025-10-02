f454d39cdf84ab5b9e60b5ab4288c6a353364f9d
serotonins <89008498+serotonins@users.noreply.github.com>
2025-10-02 22:47:53 +0900

    [Gold V] Title: 퇴사 2, Time: 348 ms, Memory: 59104 KB -BaekjoonHub

--- 백준/Gold/15486. 퇴사 2/퇴사 2.java
+++ 백준/Gold/15486. 퇴사 2/퇴사 2.java
@@ -1,5 +1,4 @@
 import java.io.IOException;
-import java.util.*;
 
 public class Main {
     static int n;
@@ -12,60 +11,22 @@ public class Main {
         return n;
     }
 
-    static class Work implements Comparable<Work> {
-        int d, t, p;
-
-        public Work(int d, int t, int p) {
-            this.d = d + 1;
-            this.t = d + t;
-            this.p = p;
-            if (this.t > n) this.p = 0;
-        }
-
-        public String toString() {
-            return "@ " + d + "일 ~ " + t + "일까지 - " + p + "원..";
-        }
-
-        @Override
-        public int compareTo(Work o) {
-            if (this.t == o.t) {
-                if (this.p == o.p) return o.d - this.d;
-                else return o.p - this.p;
-            }
-            return this.t - o.t;
-        }
-    }
-
     public static void main(String[] args) throws IOException {
         n = read();
 
-        PriorityQueue<Work> que = new PriorityQueue<>();
-        for (int i = 0; i < n; i++) {
-            que.add(new Work(i, read(), read()));
-        }
-
-        int[] dp = new int[n+1];
-        int day = 1;
-        int max = 0;
+        int[][] works = new int[n+1][2];
         for (int i = 1; i <= n; i++) {
-            Work now = que.poll();
-            int sta = now.d;
-            int due = now.t;
-            int pay = now.p;
-
-            if (due <= n) {
-                // 현재 상담 전까지 && 현재 상담 시작일 전까지
-                dp[due] = Math.max(dp[due], pay + dp[Math.min(sta-1, day)]);
-                if (max < dp[due]) {
-                    max = Math.max(max, dp[due]);
-                    for (int j = day; j < due; j++) {
-                        dp[j] = Math.max(dp[j], dp[j-1]);
-                    }
-                    day = due;
-                }
-            } else break;
+            for (int j = 0; j < 2; j++) {
+                works[i][j] = read();
+            }
         }
 
-        System.out.println(max);
+        int[] dp = new int[n+51];
+        for (int i = n; i >= 1; i--) {
+            dp[i] = Math.max(dp[i], dp[i+1]);
+            if (i + works[i][0] <= n + 1) dp[i] = Math.max(dp[i], dp[i + works[i][0]] + works[i][1]);
+        }
+
+        System.out.println(dp[1]);
     }
 }
