613dc55374fb79756e13b4b3d2fbccbd69078be3
serotonins <89008498+serotonins@users.noreply.github.com>
2025-12-04 00:01:49 +0900

    [level 1] Title: [PCCP 기출문제] 1번 / 동영상 재생기, Time: 1.05 ms, Memory: 71.9 MB -BaekjoonHub

--- 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
+++ 프로그래머스/1/340213. ［PCCP 기출문제］ 1번 ／ 동영상 재생기/［PCCP 기출문제］ 1번 ／ 동영상 재생기.java
@@ -1,29 +1,41 @@
 import java.util.*;
 
 class Solution {
-    
-    public int str2int(String str) {
-        return Integer.parseInt(str.substring(0, 2)) * 60 + Integer.parseInt(str.substring(3, 5));
+    int len, start, end;
+    int parseString2Int(String str) {
+        int m = Integer.parseInt(str.substring(0,2));
+        int s = Integer.parseInt(str.substring(3,5));
+        return m * 60 + s;
     }
-    
+    String parseInt2String(int time) {
+        int m = time / 60;
+        int s = time % 60;
+        return String.format("%02d:%02d", m, s);
+    }
+    int skipOpening(int time) {
+        return time <= end && time >= start ? end : time;
+    }
+    int next(int time) {
+        return time = Math.min(time+10, len);
+    }
+    int prev(int time) {
+        return Math.max(0, time - 10);
+    }
+    int process(String inst, int time) {
+        if (inst.equals("next")) return next(time);
+        else return prev(time);
+    }
     public String solution(String video_len, String pos, String op_start, String op_end, String[] commands) {
-        int time = str2int(pos);
-        int endTime = str2int(video_len);
-        int ops = str2int(op_start);
-        int ope = str2int(op_end);
+        len = parseString2Int(video_len);
+        start = parseString2Int(op_start);
+        end = parseString2Int(op_end);
+        int pt = parseString2Int(pos);
+        pt = skipOpening(pt);
         
-        for (String inst : commands) {
-            if (time >= ops && time < ope) time = ope;
-            if (inst.equals("prev")) {
-                time = Math.max(0, time-10);
-            } else if (inst.equals("next")) {
-                time = Math.min(endTime, time+10);
-            }
+        for (String inst: commands) {
+            pt = skipOpening(process(inst, pt));
         }
-        if (time >= ops && time < ope) time = ope;
         
-        String min = (time / 60 > 9 ? time / 60 : "0"+time / 60) + ":";
-        min += time%60 > 9 ? time%60 : "0"+time%60;
-        return min;
+        return parseInt2String(pt);
     }
 }
